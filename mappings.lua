129911427a5da33a49b047ab849de17cdd35224b
MOldtime <59734315@qq.com>
2023-09-26 16:10:30 +0800

    修改按键

--- mappings.lua
+++ mappings.lua
@@ -6,26 +6,6 @@
 return {
   -- first key is the mode
   n = {
-    -- 些官方自带的功能
-    ["<leader>c"] = {
-      function()
-        local bufs = vim.fn.getbufinfo { buflisted = true }
-        require("astronvim.utils.buffer").close(0)
-        if require("astronvim.utils").is_available "alpha-nvim" and not bufs[2] then require("alpha").start(true) end
-      end,
-      desc = "Close buffer",
-    },
-
-    -- navigate buffer tabs with `H` and `L`
-    ["<C-L>"] = {
-      function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
-      desc = "Next buffer",
-    },
-    ["<C-H>"] = {
-      function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
-      desc = "Previous buffer",
-    },
-    -- mappings seen under group name "Buffer"
     ["<leader>;p"] = {
       '"*p',
       desc = "从系统粘贴",
