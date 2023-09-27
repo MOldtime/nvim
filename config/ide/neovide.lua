4632e7fc9c723f208160aafc7f239f19145d1d27
MOldtime <59734315@qq.com>
2023-09-27 09:30:40 +0800

    update config

--- config/ide/neovide.lua
+++ config/ide/neovide.lua
@@ -1,4 +1,4 @@
-local neovideconfig = vim.g
+local g = vim.g
 vim.o.guifont = "CaskaydiaCove Nerd Font:h11"
 -- neovideconfig.neovide_theme = "dark"
 -- debug
@@ -6,11 +6,11 @@ vim.o.guifont = "CaskaydiaCove Nerd Font:h11"
 -- 没有空闲
 -- neovideconfig.neovide_no_idle = true
 -- 退出需要确认
-neovideconfig.neovide_confirm_quit = true
+g.neovide_confirm_quit = true
 -- 是否全屏
-neovideconfig.neovide_fullscreen = false
+g.neovide_fullscreen = false
 -- 记住以前窗口的大小
-neovideconfig.neovide_remember_window_size = true
+g.neovide_remember_window_size = true
 -- 使用super键位,比如<cmd>
 -- neovideconfig.neovide_input_use_logo = true
 -- 触控板死亡地带
@@ -27,13 +27,9 @@ neovideconfig.neovide_remember_window_size = true
 -- neovideconfig.neovide_cursor_vfx_particle_density = 50.0
 
 -- 设置刷新率
-neovideconfig.neovide_refresh_rate = 165
+g.neovide_refresh_rate = 165
 -- 空闲刷新率
-neovideconfig.neovide_refresh_rate_idle = 60
+g.neovide_refresh_rate_idle = 60
 -- 透明背景
 -- neovideconfig.neovide_transparency = 1.0
 -- neovideconfig.transparency = 0.9
--- 插入模式动画
--- neovideconfig.neovide_cursor_animate_in_insert_mode = true
--- 窗口模式 无法拖动位置
-neovideconfig.NEOVIDE_FRAME = "none"
--- config/vimscript.lua
+++ config/vimscript.lua
@@ -1,6 +1,3 @@
 return {
   "lan zh_CN.UTF-8", -- 设置语言为中文
-  -- "set noexpandtab", -- 不转换Tab
-  -- "set list", -- 设置Tab可见
-  -- "set listchars+=tab:---", -- 设置标志
 }
