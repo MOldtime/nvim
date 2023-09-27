3d966ed9c5083b5124d9c7682a2c630455b43bed
MOldtime <59734315@qq.com>
2023-09-27 09:56:47 +0800

    update config

--- config/ide/neovide.lua
+++ config/ide/neovide.lua
@@ -1,22 +1,24 @@
 local g = vim.g
 vim.o.guifont = "CaskaydiaCove Nerd Font:h11"
+
 -- neovideconfig.neovide_theme = "dark"
 -- debug
 -- neovideconfig.neovide_profiler = true
 -- 没有空闲
 -- neovideconfig.neovide_no_idle = true
+
 -- 退出需要确认
 g.neovide_confirm_quit = true
--- 是否全屏
-g.neovide_fullscreen = false
 -- 记住以前窗口的大小
 g.neovide_remember_window_size = true
--- 使用super键位,比如<cmd>
--- neovideconfig.neovide_input_use_logo = true
+-- 输入时隐藏鼠标
+g.neovide_hide_mouse_when_typing = true
+
 -- 触控板死亡地带
 -- neovideconfig.neovide_touch_deadzone = 4.0
 -- 触控板拖动超时
 -- neovideconfig.neovide_touch_drag_timeout = -1.17
+
 -- 开启轨道动画
 -- neovideconfig.neovide_cursor_vfx_mode = "railgun"
 -- 粒子透明度
@@ -25,11 +27,18 @@ g.neovide_remember_window_size = true
 -- neovideconfig.neovide_cursor_vfx_particle_lifetime = 1.0
 -- 粒子密度
 -- neovideconfig.neovide_cursor_vfx_particle_density = 50.0
+-- 动画时长
+g.neovide_cursor_animation_length = 0.1
+-- 光标尾部
+g.neovide_cursor_trail_size = 0.5
+-- 光标未聚焦的宽度
+-- g.neovide_cursor_unfocused_outline_width = 0.01
 
 -- 设置刷新率
 g.neovide_refresh_rate = 165
 -- 空闲刷新率
 g.neovide_refresh_rate_idle = 60
+
 -- 透明背景
 -- neovideconfig.neovide_transparency = 1.0
 -- neovideconfig.transparency = 0.9
