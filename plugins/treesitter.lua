3b64d237756c73fc9eddc6ae73abb91114985e96
MOldtime <59734315@qq.com>
2023-10-04 12:12:07 +0800

    update treesitter

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -2,8 +2,28 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
+    -- 这里可以理解为要安装的语言
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      "lua"
+      "c",
+      'cpp',
+      'rust',
+      "lua",
+      "vim",
+      "vimdoc",
+      "query",
+      'html',
+      'java',
+      'javascript',
+      'jsdoc',
+      'kotlin',
+      'typescript',
+      'css',
+      'yaml',
+      'toml',
+      'xml',
+      'json',
+      'json5',
+      'markdown',
     })
   end,
 }
