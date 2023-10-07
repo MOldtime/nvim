7a027db036fbda6fb1656fa41b0d69ddf2c705cf
MOldtime <59734315@qq.com>
2023-10-07 11:53:42 +0800

    fix markdown

--- plugins/markdown.lua
+++ plugins/markdown.lua
@@ -1,10 +1,11 @@
 return {
   {
     "iamcco/markdown-preview.nvim",
-    -- edable = false
+    -- edable = false,
     cmd = { "MarkdownPreview", "MarkdownPreviewStop" },
     lazy = false,
-    build = function() vim.fn["mkdp#util#install"]() end,
+    build = 'cd app && npm install',
+    -- build = function() vim.fn["mkdp#util#install"]() end,
     init = function() vim.g.mkdp_theme = "dark" end,
   },
 }
