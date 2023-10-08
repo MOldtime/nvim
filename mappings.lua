91c9be45b6813fff8c59dba8b12a14d67bd3f0c8
MOldtime <59734315@qq.com>
2023-10-08 18:54:54 +0800

    update

--- mappings.lua
+++ mappings.lua
@@ -1,4 +1,12 @@
+-- mnode http://yyq123.github.io/learn-vim/learn-vi-51-KeyMapping.html
 return {
+  [""] = {
+    ['<leader>`'] = { '~', desc = '把小写转换为大写'},
+    ["<M-p>"] = { "<middlemouse>", desc = "输入时粘贴系统剪切板" },
+  },
+  ['!'] = {
+    ["<M-p>"] = { "<middlemouse>", desc = "输入时粘贴系统剪切板" },
+  },
   -- first key is the mode
   n = {
     ["<M-p>"]     = { '"+p', desc = "输入时粘贴系统剪切板" },
@@ -9,10 +17,9 @@ return {
     ['<leader>r'] = {function() vim.api.nvim_command('e!') end, desc = '重新加载文件'},
   },
   v = {
-    ["J"]     = { ":m '>+1<CR>gv=gv", desc = "选择当前向下移动" },
-    ["K"]     = { ":m '<-2<CR>gv=gv", desc = "选择当前向上移动" },
+    ["J"]     = { ":m '>+1<CR>gv=gv<cr>", desc = "选择当前向下移动" },
+    ["K"]     = { ":m '<-2<CR>gv=gv<cr>", desc = "选择当前向上移动" },
     ["<M-y>"] = { '"+y', desc = "复制文字到系统" },
-    ["<M-p>"] = { "<middlemouse>", desc = "从系统粘贴" },
     ["<M-h>"] = { "^", desc = "移动到行首" },
     ["<M-l>"] = { "$", desc = "移动到行尾" },
   },
@@ -20,9 +27,6 @@ return {
   i = {
     ["<M-h>"] = { "<Esc>^i", desc = "移动到行首" },
     ["<M-l>"] = { "<Esc>$a", desc = "移动到行尾" },
-    ["<M-p>"] = { "<middlemouse>", desc = "输入时粘贴系统剪切板" },
   },
-  c = {
-    ["<M-p>"] = { "<middlemouse>", desc = "输入时粘贴系统剪切板" },
-  },
+  c = {},
 }
