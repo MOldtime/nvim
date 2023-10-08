708e781d2dc6a7fb38991fda163bbc0cedf163a4
MOldtime <59734315@qq.com>
2023-10-08 14:41:04 +0800

    修改mappings

--- config/plugin_mappings.lua
+++ config/plugin_mappings.lua
@@ -5,8 +5,8 @@ if is_available "vim-visual-multi" then
   -- visual multi
   vim.g["VM_default_mappings"] = 0
   vim.g.VM_maps = {
-    ["Add Cursor Down"] = "<M-j>",
-    ["Add Cursor Up"] = "<M-k>",
+    ["Add Cursor Down"] = "<C-j>",
+    ["Add Cursor Up"] = "<C-k>",
   }
 end
 
--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -32,20 +32,7 @@ return {
           or nil,
       }
 
-      opts.mapping = {
-        ["<Up>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
-        ["<Down>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
-        ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-        ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-        ["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-        ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-        ["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-        ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-        -- ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-        ["<C-y>"] = cmp.config.disable,
-        ["<C-e>"] = cmp.mapping { i = cmp.mapping.abort(), c = cmp.mapping.close() },
-        ["<CR>"] = cmp.mapping.confirm { select = false },
-        ["<Tab>"] = cmp.mapping(function(fallback)
+      opts.mapping['<Tab>'] = cmp.mapping(function(fallback)
           if luasnip.expand_or_jumpable() then
             luasnip.expand_or_jump()
           elseif has_words_before() then
@@ -53,15 +40,20 @@ return {
           else
             fallback()
           end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
+        end, { "i", "s" }
+      )
+      opts.mapping['<S-Tab>'] =  cmp.mapping(function(fallback)
           if luasnip.jumpable(-1) then
             luasnip.jump(-1)
           else
             fallback()
           end
-        end, { "i", "s" }),
-      }
+        end, { "i", "s" })
+      opts.mapping['<C-Space>'] = nil  
+      opts.mapping['<C-k>'] = nil  
+      opts.mapping['<C-j>'] = nil  
+      opts.mapping['<M-k>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert }
+      opts.mapping['<M-j>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert }
       return opts
     end,
   },
