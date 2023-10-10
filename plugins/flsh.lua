241963b7d9429b4f8cab2bfe68070f5fd455fa1d
Time <59734315@qq.com>
2023-10-10 13:00:34 +0800

    update mappings

--- plugins/flsh.lua
+++ plugins/flsh.lua
@@ -11,7 +11,7 @@ return {
   },
   -- stylua: ignore
     keys = {
-    { "<leader>s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
+    { "<leader><leader>", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
     -- { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
