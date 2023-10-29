7cd837f3c4de680be2e1865c9c5819174c426c3c
Time <59734315@qq.com>
2023-10-29 22:56:12 +0800

    update lua_ls set zh_cn

--- config/global.lua
+++ config/global.lua
@@ -1,5 +1,4 @@
--- ide 的检测, 并启动不同的设置
-if vim.g.neovide then require "user.config.ide.neovide" end
+if vim.g.neovide then require "user.config.ide.neovide" end -- ide 的检测, 并启动不同的设置
 
 local VimScript_list = {
   "lan zh_CN.UTF-8", -- 设置语言为中文
