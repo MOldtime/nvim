1610f6719cc28b12e2ee8fcef398527488c35630
Time <ls5474364@gmail.com>
2026-06-23 09:28:31 +0800

    feat: 适配neovide的新功能
    浮动模糊和进度条动画

--- lua/ide/neovide.lua
+++ lua/ide/neovide.lua
@@ -1,30 +1,29 @@
-if not vim.g.neovide then
-  return
-end
-vim.o.guifont = "JetBrainsMono Nerd Font,"
-  .. "JetBrainsMono Nerd Font Propo,"
-  .. "JetBrainsMonoNL Nerd Font,"
-  .. "JetBrainsMonoNL Nerd Font Propo,"
+if not vim.g.neovide then return end
+vim.o.guifont = "monospace,"
+  -- "JetBrainsMono Nerd Font,"
+  -- .. "JetBrainsMono Nerd Font Propo,"
+  -- .. "JetBrainsMonoNL Nerd Font,"
+  -- .. "JetBrainsMonoNL Nerd Font Propo,"
   .. (vim.loop.os_uname().sysname == "Windows_NT" and "Noto Sans" or "MiSans")
-  .. ":h12:#e-subpixelantialias"
+  .. ":h14:#e-subpixelantialias:#h-full"
 
-for key, value in pairs({
+for key, value in pairs {
   -- neovide_profiler = true, -- debug
   -- neovide_no_idle = true, -- 没有空闲
 
   -- neovide_confirm_quit = true -- 退出需要确认
   neovide_remember_window_size = true, -- 记住以前窗口的大小
   neovide_hide_mouse_when_typing = true, -- 输入时隐藏鼠标
-  -- neovide_refresh_rate = 165, -- 刷新率
-  -- neovide_unlink_border_highlights = true, -- 修复边框和 winbar 滚动故障
+  neovide_refresh_rate = 165, -- 刷新率
+  neovide_unlink_border_highlights = true, -- 修复边框和 winbar 滚动故障
   neovide_cursor_antialiasing = true, -- 启用或禁用光标四边形的抗锯齿功能。禁用可能会修复一些光标视觉问题。
   neovide_cursor_animate_in_insert_mode = true, -- 在插入模式下制作动画
   neovide_cursor_unfocused_outline_width = 0, -- 未聚焦的轮廓宽度
 
-  neovide_scale_factor = 1.1,
+  neovide_scale_factor = 1.0,
 
   -- neovide_scroll_animation_length = 0.3, -- 滚动动画时长
-  neovide_scroll_animation_far_lines = 999, -- 远距离滚动的行数
+  neovide_scroll_animation_far_lines = 9999, -- 远距离滚动的行数
 
   -- neovide_cursor_vfx_mode = "railgun", -- 开启轨道动画
   -- neovide_cursor_vfx_opacity = 80.0, -- 粒子透明度
@@ -36,13 +35,29 @@ for key, value in pairs({
   neovide_srgb = true,
 
   neovide_cursor_short_animation_length = 0.2,
-
-  neovide_padding_top = 5,
-  neovide_padding_right = 5,
-  neovide_padding_left = 5,
+  
+  neovide_padding_top = 10,
+  neovide_padding_bottom = 0,
+  neovide_padding_right = 10,
+  neovide_padding_left = 10,
   neovide_window_blurred = true,
   neovide_cursor_smooth_blink = true,
+
   neovide_floating_corner_radius = 0.3,
-}) do
+  neovide_floating_blur_amount_x = 10,
+  neovide_floating_blur_amount_y = 10,
+
+  neovide_progress_bar_enabled = true,
+  neovide_progress_bar_height = 5.0,
+  neovide_progress_bar_animation_speed = 200.0,
+  neovide_progress_bar_hide_delay = 0.2,
+
+  neovide_text_gamma = 0.0,
+  neovide_text_contrast = 0.5,
+  neovide_pixel_geometry = "RGBH",
+} do
   vim.g[key] = value
 end
+
+vim.opt.winblend = 50
+vim.opt.pumblend = 50
