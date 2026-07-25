1d0542f0a7878736a691232e86e913bd9b58fb2c
Time <ls5474364@gmail.com>
2026-07-25 22:47:27 +0800

    feat: 添加选择快捷键

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,6 +1,7 @@
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+require "mappings"
 
 for _, value in ipairs {
   "lan zh_CN.UTF-8", -- 设置语言为中文
